a7635547b377f5553955694d261a1381a41c7e0e
tatsushisato <starlightprayer@gmail.com>
2019-03-12 14:34:02 +0900

    create login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { FetchUserComponent } from  './fetch-user/fetch-user.component';
 import { FetchMerchantComponent } from './fetch-merchant/fetch-merchant.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: MenuComponent},
   {path: 'fuser', component: FetchUserComponent},
-  {path: 'fmerchant', component: FetchMerchantComponent}
+  {path: 'fmerchant', component: FetchMerchantComponent},
+  {path: 'login',component: LoginComponent}
 ];
 
 @NgModule({
